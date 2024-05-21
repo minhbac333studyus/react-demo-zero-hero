7d88721853da78122216d49add89ec4a70579e6b
adam <minhbac333studyus@gmail.com>
2024-05-21 08:57:14 -0400

    Nest Component Example with Hard Coded data

--- src/component/Body.tsx
+++ src/component/Body.tsx
@@ -1,9 +1,37 @@
+import {mockData} from "../utilities";
+import Address from "./Address";
+
 const Body = () => {
 
     return (
-        <div>
-            <h1>Body</h1>
-        </div>
+        <>
+            <div>
+                <h1>Body</h1>
+            </div>
+            <table>
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                </tr>
+                </thead>
+                <tbody>
+                {mockData.userList.map((user: any) => {
+                    return (
+                        <tr key = {user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.age}</td>
+                            <td>
+                                <Address/>
+                            </td>
+                        </tr>
+                    );
+                })}
+                </tbody>
+            </table>
+        </>
     )
 }
 export default Body;
